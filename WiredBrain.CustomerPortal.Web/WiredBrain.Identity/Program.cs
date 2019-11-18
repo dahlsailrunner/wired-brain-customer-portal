bd90452e39809b0edca52d2fa5dc6d8b33d71515
Erik Dahl <dahlsailrunner@yahoo.com>
2019-11-18 15:07:25 -0600

    centralized logging logic

--- WiredBrain.CustomerPortal.Web/WiredBrain.Identity/Program.cs
+++ WiredBrain.CustomerPortal.Web/WiredBrain.Identity/Program.cs
@@ -1,41 +1,27 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog.Enrichers.AspnetcoreHttpcontext;
+using WiredBrain.Logging;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.Elasticsearch;
-using Serilog.Sinks.SystemConsole.Themes;
 
 namespace WiredBrain.Identity
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("System", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.File("C:/temp/logs/flatlog-identity.json")
-                .WriteTo.Console(outputTemplate: 
-                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", 
-                    theme: AnsiConsoleTheme.Literate)
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
-                {
-                    AutoRegisterTemplate = true,
-                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-                    IndexFormat = "wired-{0:yyyy.MM.dd}",
-                    BufferBaseFilename = "C:/temp/logs/identity-buffer",
-                    InlineFields = true
-                })
-                .CreateLogger();
-
             try
             {
                 var host = CreateHostBuilder(args).Build();
-
                 Log.Information("Starting host...");
                 host.Run();
             }
@@ -54,7 +40,10 @@ namespace WiredBrain.Identity
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseSerilog();
+                    webBuilder.UseSerilog((provider, context, loggerConfig) =>
+                    {
+                        loggerConfig.WithWiredBrainConfiguration(provider, Configuration);
+                    });
                 });
     }
 }
---
+++ WiredBrain.CustomerPortal.Web/WiredBrain.Logging/SerilogHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace WiredBrain.Logging
+{
+    public static class SerilogHelper
+    {
+        public static void WithWiredBrainConfiguration(this LoggerConfiguration loggerConfig,
+            IServiceProvider provider, IConfiguration config)
+        {
+            var elasticsearchUri = config["Logging:ElasticsearchUri"];
+            var elasticIndexRoot = config["Logging:ElasticIndexFormatRoot"];
+            var rollingFileName = config["Logging:RollingFileName"];
+            var elasticBufferRoot = config["Logging:ElasticBufferRoot"];
+
+            loggerConfig
+                .ReadFrom.Configuration(config) // minimum levels defined per project in json files 
+                .Enrich.FromLogContext()
+                .WriteTo.File(rollingFileName)
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticsearchUri))
+                {
+                    AutoRegisterTemplate = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                    IndexFormat = elasticIndexRoot + "-{0:yyyy.MM.dd}",
+                    BufferBaseFilename = elasticBufferRoot,
+                    InlineFields = true
+                });
+        }
+    }
+}
